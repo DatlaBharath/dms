e4e7d7319564455cdd9c18bf0a93062e4c1977ac
D V Bharath Kamal <123236582+DatlaBharath@users.noreply.github.com>
2023-09-23 00:07:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 function App() {
   return (
-    <Header />
-    <Navbar />
-    <Home />
-  );
-}
+    <div>
+      <Header />
+      <Navbar />
+      <Home />
+    </div>
+  )
 
+}
 export default App;
