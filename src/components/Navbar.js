b2adb8dc09b18d445c7f4f25c5a62be3b7b8f065
DatlaBharath <datlabharath92@gmail.com>
2023-09-24 14:46:59 +0530

    data pulling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,11 @@ import {FaBars} from 'react-icons/fa'
 import { useState, useEffect } from "react";
 function Navbar() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [drop, setDrop] = useState(false)
+    const handleDrop = ()=>{
+        console.log(drop)
+        setDrop(!drop)
+    }
     const updateWindowWidth = () => {
     setWindowWidth(window.innerWidth);
     };
@@ -16,21 +21,29 @@ function Navbar() {
     return ( <>
         <div className="xl:px-16 lg:px-8 sm:pe-8 sm:ps-4 py-2 flex">
             <div className="flex">
-                <img src={logo} className="h-16 w-16"/>
-                <h1 className="text-[30px] max-[20px]:text-[20px] my-auto mx-2 font-semibold target:">Help N Hope</h1>
+                <img src={logo} alt="logo" className="h-16 w-16"/>
+                <h1 className="text-[30px] my-auto mx-2 font-semibold">Help N Hope</h1>
             </div>
             {windowWidth > 950 ? 
             (<div className="ms-auto flex space-x-6">
-            <a href="" className={link}>Who We Are</a>
-            <a href="" className={link}>Galary/Event</a>
-            <a href="" className={link}>Our Imapact</a>
-            <a href="" className={link}>Contact Us</a>
-            <a href="" className={link}>Login/Signup</a></div>) : (
-                <div className="ms-auto flex space-x-6"><button ><FaBars className={link}/></button></div>
+            <a href="#our" className={link}>Who We Are</a>
+            <a href="#our" className={link}>Galary/Event</a>
+            <a href="#our" className={link}>Our Imapact</a>
+            <a href="#our" className={link}>Contact Us</a>
+            <a href="#our" className={link}>Login/Signup</a></div>) : (
+                <div className="ms-auto flex space-x-6">
+                <button onClick={handleDrop}><FaBars className={link}/></button>
+                {drop && (
+                    <div>
+                    <a href="#our" className={link}>Who We Are</a>
+                    <a href="#our" className={link}>Galary/Event</a>
+                    <a href="#our" className={link}>Our Imapact</a>
+                    <a href="#our" className={link}>Contact Us</a>
+                    </div>
+                )}
+                </div>
             )
             }
-            
-            
         </div>
         </> );
 }
